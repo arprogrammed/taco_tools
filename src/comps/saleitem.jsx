2a87e620fc694540a0659a4b863a20b29e098f4a
ARprogrammed <anthony.r.programmer@gmail.com>
2023-05-31 14:39:48 -0400

    Image optimization commit

--- src/comps/saleitem.jsx
+++ src/comps/saleitem.jsx
@@ -13,7 +13,9 @@ const SaleItem = (props) => {
                 <Image src={props.img} fill={true} alt={props.name} />
                 </div>
             </div>
-            <div className={styles.coll}><Image src={props.colcImg} width={20} height={20} alt={props.collection}/>{props.collection}</div>
+            <div className={styles.coll}>
+                <Image src={props.colcImg} width={20} height={20} quality={60} placeholder={blur} alt={props.collection}/>{props.collection}
+            </div>
             <p>{props.name}</p>
             <div className={styles.price}>Price: {props.listing_price} {props.crypto}</div>
             <div className={styles.grid1}>
